4d321093cb82ec05251e7252b1929491c3428150
NChang007 <nelsonc924@gmail.com>
2022-07-02 01:05:33 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,95 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+class User(Base):
+    __tablename__ 'Users'
+    id = Column(Integer, primary_key = True, unique_key = True)
+    name = Column(String(256))
+    email = = Column(String(256))
+    user_name = Column(String(256))
+    password = Column(String(256))
+    favorites =
+
+
+class Character(Base):
+    __tablename__ = 'Character'
+    id = Column(Integer, primary_key=True, unique_key = True)
+    name = Column(String(256))
+    birth_year = Column(String(256))
+    eye_color = Column(String(256))
+    gender = Column(String(256))
+    hair_color = Column(String(256))
+    height = Column(String(256)) 
+    mass = Column(String(256))
+    skin_color = Column(String(256))
+    homeworld = Column(String(256))
+    # films array
+    # species array 
+    # starships array 
+    # vehicles array 
+
+class Starships(Base):
+    __tablename__ = 'Starships'
+    id = Column(Integer, primary_key=True, unique_key = True)
+    name = Column(String(256))
+    model = Column(String(256)) 
+    starship_class = Column(String(256))
+    manufacturer = Column(String(256))
+    cost_in_credits = Column(String(256))
+    length = Column(String(256))
+    crew = Column(String(256))
+    passengers = Column(String(256))
+    max_atmosphering_speed = Column(String(256))
+    hyperdrive_rating = Column(String(256))
+    MGLT = Column(String(256))
+    cargo_capacity = Column(String(256))
+
+class Vehicles(Base):
+    __tablename__ = "Vehicles"
+    id = Column(Integer, primary_key = True, unique_key = True)
+    name = Column(String(256))
+    model = Column(String(256))
+    vehicle_class = Column(String(256))
+    manufacturer = Column(String(256))
+    length = Column(String(256))
+    cost_in_credits = Column(String(256))
+    crew = Column(String(256))
+    passengers = Column(String(256))
+    max_atmosphering_speed = Column(String(256))
+    cargo_capacity = Column(String(256))
+
+class Planets(Base):
+    __tablename__ = "Planets"
+    id = Column(Integer, primary_key = True, unique_key = True)
+    name = Column(String(256))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(256))
+    population = Column(Integer)
+    climate = Column(String(256))
+    terrain = Column(String(256))
+    surface_water = Column(String(256))
 
     def to_dict(self):
         return {}
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
